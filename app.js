4dbad65e6b0b7a583f220fd4034e16ccc170a673
Isak Grönlund <135023164+Grizak@users.noreply.github.com>
2024-11-01 11:18:32 +0000

    Initial commit

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const express = require('express');
+require('dotenv').config();
 
+const app = express();
+const PORT = process.env.PORT || 3429;
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
